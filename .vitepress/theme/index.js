044cdba314b1ddf7ff3d2799d3ac9fb6dde71f05
萌幻妖姬 <fay1126@gmail.com>
2024-03-27 20:45:17 +0800

    初始化文档工程

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,18 @@
+// .vitepress/theme/index.js
+import { h } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
+import './custom.css'
+
+export default {
+  ...DefaultTheme,
+  Layout: h(DefaultTheme.Layout, null, {
+    'nav-bar-content-before': () => h(
+      Documate,
+      {
+        endpoint: 'http://localhost:9999/ai/askStream',
+      },
+    )
+  })
+}
